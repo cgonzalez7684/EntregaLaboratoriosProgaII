5e4c32b624c526eb264ce2716bc76cb4f044fdae
Jorge Andrés Mayorga Espinoza <32425771+Andres1421@users.noreply.github.com>
2023-06-08 20:58:54 -0600

    Entrega Andres Mayorga

--- Laboratorio#1/JorgeMayorga/main.py
+++ Laboratorio#1/JorgeMayorga/main.py
@@ -1,10 +1,13 @@
-def imprimir_numeros_pares(limite):
-    for i in range(2, limite + 1, 2):
-        print(i)
-        
-def main():
-    limite = int(input("Ingrese el límite para evaluar números pares: "))
-    imprimir_numeros_pares(limite)       
+lado = int(input("Ingrese la longitud del lado del cuadrado: "))
 
-if _name_ == "_main_":
-    main()
+num_asteriscos = lado
+
+for i in range(lado):
+    print(" *" * num_asteriscos)
+
+    if (i == 2): 
+        continue  
+          
+    
+    if (i == lado - 1):
+        break
