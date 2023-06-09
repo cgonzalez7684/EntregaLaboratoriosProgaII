fbb928d7e3d379757b949514bb23da736459aad9
Luis Aragón <111025947+luisaragon546@users.noreply.github.com>
2023-06-08 19:45:20 -0600

    LuisFernandoAragónMarín
    Laboratorio1

---
+++ Laboratorio#1/LuisFernandoAragónMarín/Laboratorio1LuisFernandoAragónMarín.py
@@ -0,0 +1,17 @@
+#def imprimir_numeros_pares(limite):
+    #for i in range(2, limite + 1, 2):
+       # print(i)
+    #Esto lo que hace es definir el limite de cuantos numeros pares se buscan 
+    
+# def main():
+    #limite = int(input("Ingrese el límite para evaluar números pares: "))
+    #imprimir_numeros_pares(limite)       
+        
+#Esta clase lo que hace es imprimir en la consola el mensaje para que el usuario cuente la cantidad de numeros pares hasta el numero ingresado
+
+
+#if __name__ == "__main__":
+    #main()
+#verifica que el programa se puede ejecutar, de poder ser ejecutado,
+#llama a la función main() para iniciar la ejecución del programa.
+
