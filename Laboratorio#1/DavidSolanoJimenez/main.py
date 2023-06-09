0cbd5949dee02c9685923d4bc708896d6dd4918b
David Solano <123708254+dasj10@users.noreply.github.com>
2023-06-08 19:49:01 -0600

    Laboratorio#1

--- Laboratorio#1/DavidSolanoJimenez/main.py
+++ Laboratorio#1/DavidSolanoJimenez/main.py
@@ -1,16 +1,6 @@
-def es_primo(numero):
-    '''Función para verificar si un número es primo'''
-    if numero < 2:
-        return False
-    for i in range(2, numero):
-        if numero % i == 0:
-            return False
-    return True
-
-limite = int(input("Ingrese el límite para evaluar números primos: "))
-
-print("Números primos hasta el límite", limite, ":")
-#2 ... 10 2-3-4-5
-for num in range(2, limite + 1):
-    if es_primo(num):
-        print(num)
+def numeros_pares(limite):
+    for i in range(2,limite+1,2):
+        print(i)
+        
+limite = int(input("ingrese el numero limite para evaluar pares: "))
+numeros_pares(limite)
