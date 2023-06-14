5e0fdf13f4946af6369ec706246218de40dd224f
Jose Emilio Maroto Bolaños <joseemiliomb2220@gmail.com>
2023-06-13 18:56:30 -0600

    Entrega Laboratorio 1

--- Laboratorio#2/JoseEmilioMarotoBolaños/main.py
+++ Laboratorio#2/JoseEmilioMarotoBolaños/main.py
@@ -1,1 +1,14 @@
-print("Hola Mundo.")
+lado = int(input("Ingrese la longitud del lado del cuadrado: "))
+
+# Variable pára controlar el numero de asteriscos en cada fila
+num_asteristicos = lado
+
+# Bucle while para dibujar el cuadrado
+while lado > 1:
+    # Imprimir una fila de asteriscos
+    print("*" * num_asteristicos)
+    
+    if (lado ==3):
+        continue
+    
+    if(lado )
