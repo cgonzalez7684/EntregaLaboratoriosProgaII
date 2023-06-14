31cc805fa7a572b89b20cd31c531db8b8ab75748
“[David <[skate1jose@gmail.com]>
2023-06-14 10:53:51 -0600

    Laboratorio#2/Jose David Arias

--- Laboratorio#2/JoseDavidAriasSoto/main.py
+++ Laboratorio#2/JoseDavidAriasSoto/main.py
@@ -1,1 +1,8 @@
-print("Hola Munndo + 4")
+# Ingresar la longitud del lado del cuadrado
+longitud_lado = int(input("Ingrese la longitud del lado del cuadrado: "))
+
+# Imprimir cuadrado de asteriscos
+for i in range(longitud_lado):
+    for j in range(longitud_lado):
+        print("*", end=" ")
+    print()
