8b45286ea8e12ae964ce8278f17562a6d2114546
KevinAV <kevinvalverde747@gmail.com>
2023-06-15 16:41:18 -0600

    Entrega Laboratorio

--- Laboratorio#2/KevinAriasValverde/Main.py
+++ Laboratorio#2/KevinAriasValverde/Main.py
@@ -1,1 +1,14 @@
-print ("Prueba")
+'''Correcion de errores en el codigo'''
+
+lado = int(input("Ingrese la longitud del lado del cuadrado: "))
+
+# Variable para controlar el número de asteriscos en cada fila
+num_asteriscos = lado
+
+# Bucle while para dibujar el cuadrado
+while lado >= 1:
+    # Imprimir una fila de asteriscos
+    print("*" * num_asteriscos)
+    
+    # Decrementar el valor del lado para la siguiente fila
+    lado = lado - 1
