10f89ba55b0a7bdfd63ce1ff57ef1249348705d1
Jonathan Ramirez <jonathanrr.2557.jr@gmail.com>
2023-06-15 14:23:46 -0600

    Entrega Laboratorio 1

--- Laboratorio#1/JonathanRamirezRamirez/Main.py
+++ Laboratorio#1/JonathanRamirezRamirez/Main.py
@@ -1,1 +1,13 @@
-jhbj
+def es_par(numero):
+    if numero < 2:
+        return True
+    for i in range(2, numero):
+        if numero % 2 == 0:
+            return True
+    return False
+limite = int(input("Ingrese el límite para números pares: "))
+print("Números pares hasta el límite", limite, ":")
+for num in range(2, limite + 1):
+    if es_par(num):
+        print(num)
+        
