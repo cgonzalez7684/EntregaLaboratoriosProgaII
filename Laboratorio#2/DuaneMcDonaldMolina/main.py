f07276c490812aaf58807b1519814ed3d502f5b1
duanezxz <duanemc_164@hotmail.com>
2023-06-15 12:37:14 -0600

    Access

--- Laboratorio#2/DuaneMcDonaldMolina/main.py
+++ Laboratorio#2/DuaneMcDonaldMolina/main.py
@@ -1,1 +1,18 @@
-Get it gear
+side =  int(input("Acceda la longitud del lado del cuadrado:"))
+
+#variable para controlar el numero asteriscos en cada fila
+num_asterisk = side
+
+#bucle while para dibujar el cuadrado
+while side > 0:
+    #imprimir una linea de asteriscos
+    print("* " *num_asterisk) 
+    
+    #if (side == 3):
+        #contine
+    
+    if (side==0):
+        break
+    #Decrementa el valor  lados para siguiente fila
+    side -=1  
+        
