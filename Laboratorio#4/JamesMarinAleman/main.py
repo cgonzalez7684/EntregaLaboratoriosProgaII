6dd00476ff7e4167f683ec25e1519d62b6c27941
“[James <[jamesmarinaleman@gmail.com]>
2023-06-18 17:13:02 -0600

    Lab_4_JamesMarinAleman

--- Laboratorio#4/JamesMarinAleman/main.py
+++ Laboratorio#4/JamesMarinAleman/main.py
@@ -10,7 +10,9 @@ print("Opcion #3 ---> Reemplazar ''")
 try:
     #while para hacer que si ingresa algo que no sea una letra vuelva a digitar una de las opciones
     while True:
+        print()
         tecla = input("Digitar opcion: ")
+        print()
         if (not(tecla.isdigit())):
             print('Debe digitar un numero')
             continue
@@ -18,21 +20,26 @@ try:
         i = int(tecla)
         if i ==1:
             texto=("San José; Cartago; Puntarenas; Heredia; Limón; Alajuela; Guanacaste")
-            
-            x = texto.count("a")
-            
-            print (texto)
-            
-            
-            
-            
+            contdor_a = texto.count("a") # .count cuenta la cantidad de letras de un str pero si le colocamos (a) solo lo hara de a
+              
+            print("El siguiente texto --> "+texto+" tiene :"+str(contdor_a) +" a")
+
         elif i ==2:
             
-            print(" opcion 2 ")
+            texto = (input("Favor ingrese un texto"+ "\n"))
+            trasnformar_ES = texto.replace('es','ES')#replace reemplaza el texto
+            
+            print("El texto fue trasformado :"+ "\n" + trasnformar_ES )
+            
             
         elif i ==3:
+             
+            texto = (input("Favor ingrese un texto"+ "\n"))
+            mayuscular = texto.upper()#replace reemplaza el texto
             
-            print(" opcion 3 ")
+            print("El texto fue trasformado :"+ "\n" + mayuscular )
+            
+            
             
         elif i ==4:
             print("Se cierra el programa")  #para cerrar sistemma del todo
