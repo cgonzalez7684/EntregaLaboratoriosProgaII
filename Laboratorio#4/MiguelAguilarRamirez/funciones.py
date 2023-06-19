a010ad86999fa122786174cce7d016ccf8b06125
Miguel Aguilar <maguilar1650@gmail.com>
2023-06-19 13:10:16 -0600

    laboratorio 3 y 4 comentados

--- Laboratorio#4/MiguelAguilarRamirez/funciones.py
+++ Laboratorio#4/MiguelAguilarRamirez/funciones.py
@@ -8,26 +8,25 @@ def menu():
 
 def programa():
     while True:
-        menu()
+        menu() #aquí se hace el llamado a la funcion de un pequeño menu que se crea 
         opcion = input("Elija una opción del menu: ")
-        if (not(opcion.isdigit())):
+        if (not(opcion.isdigit())): # se utiliza esta funcion para verificar que se ingresen numeros y cadenas de texto
             print("Opcion invalida debe dijitar un numero","\n")
-            continue
+            continue 
         i = int(opcion)
         if i == 1:
             provincias = "San José; Cartago; Puntarenas; Heredia; Limón; Alajuela; Guanacaste"
             print(provincias)
-            print("La cantidad de letras (a) que hay en el texto son", provincias.count("a"),"\n")
+            print("La cantidad de letras (a) que hay en el texto son", provincias.count("a"),"\n") # metodo par contar la cantidad de veses que coincida el parametro que le enviemos
         elif i ==2:
             texto = input("Ingrese el texto: ")
-            print(texto.replace("es", "ES"),"\n")
+            print(texto.replace("es", "ES"),"\n") # uso este metodo para remplazar un fragmento de la cadena por otra dada
         elif i ==3:
             texto = input("Ingrese el texto: ")
-            print(texto.upper(),"\n")
+            print(texto.upper(),"\n") # aqui uamos este metodo par convertir todo a mayuscula
         elif i == 4:
             break
         else:
                 print('Opcion invalida',"\n")
                 continue
             
-programa()
