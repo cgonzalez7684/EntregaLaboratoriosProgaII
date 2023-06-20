024911eaa5a9e057344c78549d19184634d0ede5
Jose Emilio Maroto Bolaños <joseemiliomb2220@gmail.com>
2023-06-20 10:28:43 -0600

    Entrega Laboratorio#3

--- Laboratorio#3/JoseEmilioMarotoBolaños/main.py
+++ Laboratorio#3/JoseEmilioMarotoBolaños/main.py
@@ -1,10 +1,9 @@
 def elevarNumeroAlaPotencia2(numero, potencia):
     calculo = numero ** potencia
     return calculo
-
-
+#se le asigna los valores
 resultado = elevarNumeroAlaPotencia2(numero = 2, potencia = 3)
-
+#se crea una etiqueta para saber que se va imprimir
 etiqueta = "Elevar a {0} a la potencia {1} da como resultado {2}"
-
-print(etiqueta.format(2,2,resultado))
+#asignamos la etiqueta con los valores
+print(etiqueta.format(2,3,resultado))
