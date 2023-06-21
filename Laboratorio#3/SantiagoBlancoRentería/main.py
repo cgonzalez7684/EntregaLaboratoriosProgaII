fb0296bd8b97ab430d9f5ff7680be95dfe769e9e
Santiago Blanco Rentería <134663445+santiblanco01@users.noreply.github.com>
2023-06-21 16:55:02 -0600

    Entrega lab 3 (definitiva)

--- Laboratorio#3/SantiagoBlancoRentería/main.py
+++ Laboratorio#3/SantiagoBlancoRentería/main.py
@@ -7,5 +7,5 @@ resultado = elevarNumeroAlaPotencia2(numero = 2, potencia = 3)
 
 etiqueta = "Elevar {0} a la potencia {1} da como resultado {2}"
 
-#El error era en este print. Nada más cambié el número incorrecto(2) por el correcto(3).
+#El error era en este print. Lo que hice fue cambiar el número incorrecto(2) por el correcto(3)
 print (etiqueta.format(2,3,resultado))
