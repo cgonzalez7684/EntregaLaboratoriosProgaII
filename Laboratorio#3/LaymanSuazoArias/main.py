43a431bbed79728d36488523b17c14b39ed1e18b
Layman Suazo <laymansuazo@gmail.com>
2023-06-22 06:43:40 -0600

    lab 3

--- Laboratorio#3/LaymanSuazoArias/main.py
+++ Laboratorio#3/LaymanSuazoArias/main.py
@@ -1,8 +1,11 @@
+# Funcion para calcular potencia
 def elevarNumeroAlaPotencia2(numero, potencia):
     calculo = numero ** potencia
     return calculo
 
-
+# llamado de la funcion
 resultado = elevarNumeroAlaPotencia2(numero = 2, potencia = 3)
+# Impresion usando el metodo format
 etiqueta = "Elevar {0} a la potencia {1} da como resultado {2}"
+# Pasando variable para impresion
 print(etiqueta.format(2,3,resultado))
