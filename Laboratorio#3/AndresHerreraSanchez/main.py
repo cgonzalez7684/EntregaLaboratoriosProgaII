e78950724773e2f3db6bcdc489b5053238f7edc0
Andresherrera122 <135289044+Andresherrera122@users.noreply.github.com>
2023-06-22 14:03:41 -0600

    laboratorio 3,4

---
+++ Laboratorio#3/AndresHerreraSanchez/main.py
@@ -0,0 +1,9 @@
+def elevarNumeroAlaPotencia2(numero, potencia):
+    calculo = numero ** potencia
+    return calculo
+
+resultado = elevarNumeroAlaPotencia2(numero = 2, potencia = 3)
+
+etiqueta = "Elevar {0} a la potencia {1} da como resultado {2}"
+
+print(etiqueta.format(2, 3, resultado))   #corregimos el error donde se tomaba numero 2 veces
