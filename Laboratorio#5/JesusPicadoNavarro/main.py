2645d2cf36af8158e7824ad666e7d5302b991ae2
“[Jesus <[Jesuspicado70@gmail.com]>
2023-06-25 12:32:35 -0600

    Entrega lab5

---
+++ Laboratorio#5/JesusPicadoNavarro/main.py
@@ -0,0 +1,8 @@
+
+import Clase as prog #importa la clase Clase() a la clase main
+def main():#define main para ejecutar el programa
+    prog()
+
+
+if __name__ == '__main__':#define la clase main
+    main()
