2879f3290f7dba667caa7e2f8989054891722c3c
“Valeria <valeriacarvajal1@outlook.com>
2023-06-27 20:17:41 -0600

    Entrega de laboratorio No. 5

---
+++ Laboratorio#5/valeriaCarvajalMayorga/main.py
@@ -0,0 +1,3 @@
+import funciones #importo el modulo funciones donde se encuentra la creación de la lista, métodos y menú
+
+funciones.menu_de_opciones() #la función menu_de_opciones contiene todas las funciones solicitadas
